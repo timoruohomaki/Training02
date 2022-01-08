d20977bed93bd87deab21ecc9a37a6c0a99322bc
Timo Ruohomaki <timo.ruohomaki@forumvirium.fi>
2022-01-08 14:11:24 +0200

    Update first.go

--- src/first.go
+++ src/first.go
@@ -2,8 +2,33 @@ package main
 
 import "fmt"
 
+// OPTION 1: PASSING ARRAY POINTERS
+
+/*
+
+func foo(x *[3]int) int {
+	(*x)[0] = (*x)[0] + 1
+	return x[0]
+}
+
 func main() {
+	a := [3]int{1, 2, 3}
+	foo(&a)
+	fmt.Println(a)
 
-	fmt.Println("Hello")
+}
 
+*/
+
+// OPTION 2: PASSING SLICES INSTEAD
+
+func foo(sli []int) int {
+	sli[0] = sli[0] + 1
+	return sli[0]
+}
+
+func main() {
+	a := []int{1, 2, 3}
+	foo(a)
+	fmt.Println(a)
 }
